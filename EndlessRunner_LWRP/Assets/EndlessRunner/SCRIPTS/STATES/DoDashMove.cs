4a41675c9036c94f9b06e69d3c67ae88a401d5b5
SAMGameDev <42099858+SAMGameDev@users.noreply.github.com>
2019-12-26 12:12:00 +0500

    Dash Move Improved
    Dash Move Improved, New Dash Animation, Camera Jitter During Dash Fixed, More small bugs Fixed

--- EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/DoDashMove.cs
+++ EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/DoDashMove.cs
@@ -11,9 +11,6 @@ namespace RunnerGame
         protected float DashForce;
         public override void OnEnter(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
-            CharacterControl control = playerStateBase.GetCharacterControl(animator);
-            //control.RIGIDBODY.velocity = new Vector3(0f, control.RIGIDBODY.velocity.y, DashForce);
-           // control.RIGIDBODY.AddForce(Vector3.forward * DashForce, ForceMode.Force);
         }
         public override void OnUpdate(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
