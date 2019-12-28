4306728e3f5be63f0ed28c123f0724da6ac7ea63
SAMGameDev <42099858+SAMGameDev@users.noreply.github.com>
2019-12-28 11:29:18 +0500

    Update RotateOnSlope.cs

--- EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/RotateOnSlope.cs
+++ EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/RotateOnSlope.cs
@@ -7,9 +7,6 @@ namespace RunnerGame
     [CreateAssetMenu(fileName = "New Object", menuName = "ScriptableObject/Ability/RotateOnSlope")]
     public class RotateOnSlope : ScriptableObjectData
     {
-        [SerializeField]
-        protected float smoothRottation;
-
         public override void OnEnter(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
 
@@ -21,12 +18,12 @@ namespace RunnerGame
             RaycastHit hit;
 
             Ray ray = new Ray(control.transform.position, -control.transform.up);
-            if (Physics.Raycast(ray, out hit, 5f))
+            if (Physics.Raycast(ray, out hit, 0.3f))
             {
                 control.transform.rotation = Quaternion.LookRotation(Vector3.Cross(control.transform.right, hit.normal));
                 //control.transform.rotation = Quaternion.Slerp(control.transform.rotation, rot, smoothRottation * Time.deltaTime);
             }
-            Debug.DrawRay(control.transform.position, -control.transform.up * 0.7f, Color.white);
+           // Debug.DrawRay(control.transform.position, -control.transform.up * 0.7f, Color.white);
         }
 
         public override void OnExit(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
