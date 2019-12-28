d90d9d3bff0dfd49e8fce45280e813b074189f08
SAMGameDev <42099858+SAMGameDev@users.noreply.github.com>
2019-12-28 10:52:27 +0500

    minor changes

--- EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/RotateOnSlope.cs
+++ EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/RotateOnSlope.cs
@@ -23,8 +23,8 @@ namespace RunnerGame
             Ray ray = new Ray(control.transform.position, -control.transform.up);
             if (Physics.Raycast(ray, out hit, 5f))
             {
-                Quaternion rot = Quaternion.LookRotation(Vector3.Cross(control.transform.right, hit.normal));
-                control.transform.rotation = Quaternion.Slerp(control.transform.rotation, rot, smoothRottation * Time.deltaTime);
+                control.transform.rotation = Quaternion.LookRotation(Vector3.Cross(control.transform.right, hit.normal));
+                //control.transform.rotation = Quaternion.Slerp(control.transform.rotation, rot, smoothRottation * Time.deltaTime);
             }
             Debug.DrawRay(control.transform.position, -control.transform.up * 0.7f, Color.white);
         }
