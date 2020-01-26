635e36d7e57c3bb0ecc5caf8d63e29af1862976f
SAMGameDev <42099858+SAMGameDev@users.noreply.github.com>
2020-01-26 19:32:34 +0500

    Update DashMove.cs

--- EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/DashMove.cs
+++ EndlessRunner_LWRP/Assets/EndlessRunner/SCRIPTS/STATES/DashMove.cs
@@ -12,7 +12,6 @@ namespace RunnerGame
         public override void OnEnter(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
             playerStateBase.characterControl.RIGIDBODY.velocity = Vector3.zero;
-
         }
         public override void OnUpdate(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
@@ -23,7 +22,6 @@ namespace RunnerGame
         public override void OnExit(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
             animator.SetBool(TranistionParemeters.Dash.ToString(), false);
-
         }
     }
 
