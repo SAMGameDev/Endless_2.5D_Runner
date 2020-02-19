0bb5a2a49bfb8866e390ccbcc902e726d7a6a2a7
SAMGameDev <42099858+SAMGameDev@users.noreply.github.com>
2020-02-19 20:14:26 +0500

    fixed

--- EndlessRunner_LWRP/Assets/EndlessRunner_MYGAME/SCRIPTS/CHARACTERCONTROL/CharacterControl.cs
+++ EndlessRunner_LWRP/Assets/EndlessRunner_MYGAME/SCRIPTS/CHARACTERCONTROL/CharacterControl.cs
@@ -72,11 +72,6 @@ namespace RunnerGame
                 c.characterControl = this;
             }
         }
-
-        private void Update()
-        {
-            Time.timeScale = 0.2f;
-        }
         private void FixedUpdate()
         {
             ApplyGravity();
