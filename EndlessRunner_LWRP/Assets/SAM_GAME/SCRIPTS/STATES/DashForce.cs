f3cd730a1104bf703e96d28cb3e62c05cfddc5e5
SAMGameDev <42099858+SAMGameDev@users.noreply.github.com>
2020-07-01 20:49:01 +0500

    some fixes

--- EndlessRunner_LWRP/Assets/SAM_GAME/SCRIPTS/STATES/DashForce.cs
+++ EndlessRunner_LWRP/Assets/SAM_GAME/SCRIPTS/STATES/DashForce.cs
@@ -28,7 +28,6 @@ namespace RunnerGame
         public override void OnExit(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
             playerStateBase.characterControl.RIGIDBODY.useGravity = true;
-            playerStateBase.characterControl.RIGIDBODY.velocity = Vector3.zero;
         }
     }
 }
