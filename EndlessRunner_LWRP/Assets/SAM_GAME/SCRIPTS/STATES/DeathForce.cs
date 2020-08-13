2121b64164cfdd36d545b7e54d2e1d1121550dd5
SAMGameDev <42099858+SAMGameDev@users.noreply.github.com>
2020-08-13 19:02:32 +0500

    Update

--- EndlessRunner_LWRP/Assets/SAM_GAME/SCRIPTS/STATES/DeathForce.cs
+++ EndlessRunner_LWRP/Assets/SAM_GAME/SCRIPTS/STATES/DeathForce.cs
@@ -5,15 +5,19 @@ namespace EndlessRunning
     [CreateAssetMenu(fileName = "New Object", menuName = "ScriptableObject/Ability/DeathForce")]
     public class DeathForce : ScriptableObjectData
     {
+        [Range(0.01f, 1f)]
+        public float animationProgres;
         public override void OnEnter(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
             CameraManger.Instance.ShakeCamera(0.3f);
+            playerStateBase.characterControl.cCollider.isTrigger = true;
             playerStateBase.characterControl.Death = false;
             animator.SetBool(HashManger.Instance.DicMainParameters[TranistionParemeters.Die], false);
-            
-            if (!playerStateBase.characterControl.RIGIDBODY.useGravity)
+
+            if (playerStateBase.characterControl.RIGIDBODY.useGravity
+                && stateInfo.normalizedTime >= animationProgres)
             {
-                playerStateBase.characterControl.RIGIDBODY.useGravity = true;
+                playerStateBase.characterControl.RIGIDBODY.useGravity = false;
             }
         }
         public override void OnUpdate(PlayerStateBase playerStateBase, Animator animator, AnimatorStateInfo stateInfo)
