4828ca5ccf931277ae6343ce30f87c4bf2d80e2f
SamGame_Dev <42099858+SAMGameDev@users.noreply.github.com>
2020-10-19 14:41:05 +0500

    Update TakeInputs.cs

--- EndlessRunner_LWRP/Assets/SAM_GAME/SCRIPTS/INPUTSCRIPTS/TakeInputs.cs
+++ EndlessRunner_LWRP/Assets/SAM_GAME/SCRIPTS/INPUTSCRIPTS/TakeInputs.cs
@@ -19,41 +19,27 @@ namespace EndlessRunning
         {
             if (characterControl.isStarted)
             {
-                if (Input.GetMouseButtonUp(0)
-                   && Input.mousePosition.y >= splitScreenY
-                   && Input.mousePosition.x <= splitScreenX
-                   || Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetMouseButtonUp(0))
                 {
                     if (!characterControl.StartRun)
                     {
                         characterControl.StartRun = true;
                     }
-                    else
+
+                    if (Input.mousePosition.y >= splitScreenY &&
+                        Input.mousePosition.x <= splitScreenX
+                        || Input.GetKeyDown(KeyCode.Space))
                     {
                         characterControl.Jump = true;
                     }
-                }
-                else if (Input.GetMouseButtonUp(0) && Input.mousePosition.y < splitScreenY && Input.mousePosition.x <= splitScreenX || Input.GetKeyDown(KeyCode.DownArrow))
-
-                {
-                    if (!characterControl.StartRun)
-                    {
-                        characterControl.StartRun = true;
-                    }
-                    else
+                    else if (Input.mousePosition.y < splitScreenY &&
+                    Input.mousePosition.x <= splitScreenX
+                    || Input.GetKeyDown(KeyCode.DownArrow))
                     {
                         characterControl.Slide = true;
                         StartCoroutine(TurnOff(0.25f));
                     }
-                }
-                else if (Input.GetMouseButtonUp(0) && Input.mousePosition.x > splitScreenX || Input.GetKeyDown(KeyCode.RightArrow))
-
-                {
-                    if (!characterControl.StartRun)
-                    {
-                        characterControl.StartRun = true;
-                    }
-                    else
+                    else if (Input.mousePosition.x > splitScreenX || Input.GetKeyDown(KeyCode.RightArrow))
                     {
                         characterControl.Dash = true;
                     }
@@ -63,9 +49,53 @@ namespace EndlessRunning
                     characterControl.Jump = false;
                     characterControl.Dash = false;
                 }
+                #region old code
+                //if (Input.GetMouseButtonUp(0)
+                //   && Input.mousePosition.y >= splitScreenY
+                //   && Input.mousePosition.x <= splitScreenX
+                //   || Input.GetKeyDown(KeyCode.Space))
+                //{
+                //    if (!characterControl.StartRun)
+                //    {
+                //        characterControl.StartRun = true;
+                //    }
+                //    else
+                //    {
+                //        characterControl.Jump = true;
+                //    }
+                //}  
+                //else if (Input.GetMouseButtonUp(0) &&
+                //    Input.mousePosition.y < splitScreenY &&
+                //    Input.mousePosition.x <= splitScreenX
+                //    || Input.GetKeyDown(KeyCode.DownArrow))
+
+                //{
+                //    if (!characterControl.StartRun)
+                //    {
+                //        characterControl.StartRun = true;
+                //    }
+                //    else
+                //    {
+                //        characterControl.Slide = true;
+                //        StartCoroutine(TurnOff(0.25f));
+                //    }
+                //}
+                //else if (Input.GetMouseButtonUp(0)
+                //    && Input.mousePosition.x > splitScreenX
+                //    || Input.GetKeyDown(KeyCode.RightArrow))
+                //{
+                //    if (!characterControl.StartRun)
+                //    {
+                //        characterControl.StartRun = true;
+                //    }
+                //    else
+                //    {
+                //        characterControl.Dash = true;
+                //    }
+                //}
+                #endregion
             }
         }
-
         private IEnumerator TurnOff(float time)
         {
             yield return new WaitForSeconds(time);
