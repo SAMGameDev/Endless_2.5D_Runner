9e79a89b0061fb21c29c4928eac6268b02b6d80d
SamGame_Dev <42099858+SAMGameDev@users.noreply.github.com>
2020-11-24 21:14:08 +0500

    Jane Animation Setup Others Broke

--- EndlessRunner_LWRP/Assets/SAMGAME/SCRIPTS/CHARACTERCONTROL/FPSDisplay.cs
+++ EndlessRunner_LWRP/Assets/SAMGAME/SCRIPTS/CHARACTERCONTROL/FPSDisplay.cs
@@ -8,7 +8,7 @@ namespace EndlessRunning
 
         private void Start()
         {
-            Time.timeScale = 1f;
+            Time.timeScale = 0.1f;
         }
         private void Update()
         {
